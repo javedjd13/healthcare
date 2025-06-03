d068bf902b9f2670b9798a2ee06efc5150c4f9ee
javed ansari <javed13jd@gmail.com>
2025-06-04 00:33:06 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import SignUp from "./components/Auth/pages/SignUp";
 import Login from "./components/Auth/pages/Login";
 import { useState } from "react";
 const App = () => {
-  const [mode, setMode] = useState("signup"); // 'signup' or 'login'
+  const [mode, setMode] = useState("signup"); // 'signup' & 'login'
   return (
     <div className="">
       <div className="min-h-screen  flex items-center justify-center bg-[#009DDE]">
